ae275b520602d57bb56390643f6753ee89d17c81
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2018-10-11 20:14:50 +0800

    Maybe this will work?

--- calendarview/src/main/java/com/haibin/calendarview/MonthViewPager.java
+++ calendarview/src/main/java/com/haibin/calendarview/MonthViewPager.java
@@ -52,10 +52,12 @@ public final class MonthViewPager extends ViewPager {
 
     public MonthViewPager(Context context) {
         this(context, null);
+        initPageTransformer();
     }
 
     public MonthViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initPageTransformer();
     }
 
     /**
@@ -360,16 +362,17 @@ public final class MonthViewPager extends ViewPager {
         }
     }
 
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        return mDelegate.isMonthViewScrollable() && super.onTouchEvent(ev);
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercepted = super.onInterceptTouchEvent(swapXY(ev));
+        swapXY(ev); // return touch coordinates to original reference frame for any child views
+
+        return mDelegate.isMonthViewScrollable() && intercepted;
     }
 
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return mDelegate.isMonthViewScrollable() && super.onInterceptTouchEvent(ev);
+    public boolean canScrollHorizontally(int direction) {
+        return mDelegate.isMonthViewScrollable() && super.canScrollHorizontally(direction);
     }
 
     @Override
@@ -435,5 +438,56 @@ public final class MonthViewPager extends ViewPager {
         }
     }
 
+    private void initPageTransformer() {
+        // The majority of the magic happens here
+        setPageTransformer(true, new VerticalPageTransformer());
+        // The easiest way to get rid of the overscroll drawing that happens on the left and right
+        setOverScrollMode(OVER_SCROLL_NEVER);
+    }
 
+    private class VerticalPageTransformer implements ViewPager.PageTransformer {
+
+        @Override
+        public void transformPage(View view, float position) {
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+                view.setAlpha(1);
+
+                // Counteract the default slide transition
+                view.setTranslationX(view.getWidth() * -position);
+
+                //set Y position to swipe in from top
+                float yPosition = position * view.getHeight();
+                view.setTranslationY(yPosition);
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+
+    /**
+     * Swaps the X and Y coordinates of your touch event.
+     */
+    private MotionEvent swapXY(MotionEvent ev) {
+        float width = getWidth();
+        float height = getHeight();
+
+        float newX = (ev.getY() / height) * width;
+        float newY = (ev.getX() / width) * height;
+
+        ev.setLocation(newX, newY);
+
+        return ev;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return super.onTouchEvent(swapXY(ev));
+    }
 }
