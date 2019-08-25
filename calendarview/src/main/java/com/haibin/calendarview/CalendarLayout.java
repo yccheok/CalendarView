7030575702e78fca696125c51e21cf78f980dec1
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2019-08-25 16:48:52 +0800

    1.13

--- calendarview/src/main/java/com/haibin/calendarview/CalendarLayout.java
+++ calendarview/src/main/java/com/haibin/calendarview/CalendarLayout.java
@@ -502,7 +502,7 @@ public class CalendarLayout extends LinearLayout {
             mContentView.measure(widthMeasureSpec, heightSpec);
             try {
                 mContentView.layout(mContentView.getLeft(), mContentView.getTop(), mContentView.getRight(), mContentView.getBottom());
-            } catch (ArrayIndexOutOfBoundsException | IndexOutOfBoundsException e) {
+            } catch (IndexOutOfBoundsException e) {
                 e.printStackTrace();
             }
             return;
